73d7f34fb21ea1b2e9deb435b7db0c689070ca85
Caleb <calebkiune@gmail.com>
2024-10-06 21:03:40 +0300

    Adds project files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('create-task-form');
+  const taskList = document.getElementById('tasks');
+
+  form.addEventListener('submit', function(event) {
+      event.preventDefault(); 
+
+      const taskDescription = document.getElementById('new-task-description').value;
+      if (taskDescription) {
+          const newTask = document.createElement('li');
+          newTask.textContent = taskDescription;
+
+          const deleteButton = document.createElement('button');
+          deleteButton.textContent = 'x';
+          deleteButton.addEventListener('click', function() {
+              taskList.removeChild(newTask);
+          });
+
+          newTask.appendChild(deleteButton);
+          taskList.appendChild(newTask);
+
+          document.getElementById('new-task-description').value = '';
+      }
+  });
 });
